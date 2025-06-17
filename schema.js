aa0ee042cda3ec127e99a92455f62c099fb1e36d
prasannakumar96 <prasannakumar96421@gmail.com>
2025-06-17 17:22:47 +0530

    added schema

---
+++ schema.js
@@ -0,0 +1,119 @@
+const UserDetails = {
+  userName: String,
+  firstName: String,
+  middleName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  professionalSummary: String,
+  profile: String,
+  DOB: String,
+  addresses: {
+    permanentAddress: {
+      doorNo: String,
+      streetName: String,
+      landMark: String,
+      areaName: String,
+      pincode: String,
+      cityName: String,
+      districtName: String,
+      stateName: String,
+    },
+  },
+  Skills: [String],
+  languages: [String],
+  hobbies: [String],
+  extraCirrcularActivities: [String],
+  educationDetails: [
+    {
+      type: String, // 10th
+      board: String,
+      school: String,
+      hallticket: String,
+      startDate: String,
+      endDate: String,
+      yearofPass: String,
+      gradingSystem: String, // cgpa / %
+      grade: String,
+      city: String,
+      description: String,
+      stream: String, // Mech / cse
+    },
+  ],
+  experiences: [
+    {
+      company: String, // Company Name
+      role: String,
+      startDate: String,
+      endDate: String,
+      city: String,
+      description: String,
+    },
+  ],
+  links: [
+    {
+      title: String, // like github
+      link: String,
+    },
+  ],
+  projects: [
+    {
+      company: String, // Company Name
+      project: String,
+      startDate: String,
+      endDate: String,
+      city: String,
+      description: String,
+    },
+  ],
+  internships: [
+    {
+      company: String, // Company Name
+      project: String,
+      startDate: String,
+      endDate: String,
+      city: String,
+      description: String,
+    },
+  ],
+  volunteerings: [
+    {
+      organization: String, // Company Name
+      volunteering: String,
+      startDate: String,
+      endDate: String,
+      city: String,
+      description: String,
+    },
+  ],
+  accomplishments: [
+    {
+      company: String, // Company Name
+      accomplishment: String,
+      startDate: String,
+      endDate: String,
+      city: String,
+      description: String,
+    },
+  ],
+  research: [
+    {
+      organization: String,
+      type: String,
+      startDate: String,
+      endDate: String,
+      description: String,
+      title: String,
+    },
+  ],
+  certifications: [
+    {
+      organization: String,
+      title: String,
+      type: String,
+      startDate: String,
+      endDate: String,
+      description: String,
+    },
+  ],
+};
